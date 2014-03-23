9af72f73824bc6efc4ad6bff6e331a2c19e69a23
Pablo Padilla <pgpb.padilla@gmail.com>
2014-03-22 20:21:14 -0700

    As a user I want to be able to delete todo's

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,12 @@ angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['one', 'two', 'three'];
 
-    $scope.addTodo= function(){
+    $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
-      $scope.todo= '';
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
     };
   });
